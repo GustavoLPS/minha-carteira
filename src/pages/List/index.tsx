9411419c658c544530e5b39fa6a227fdd85854d0
Gustavo Pinheiro <gustavo.eng18@gmail.com>
2020-11-26 17:57:14 -0400

    first commit

---
+++ src/pages/List/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const List: React.FC = () => {
+    return (
+        <div>
+            <h1>List</h1>
+        </div>
+    )
+}
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SignIn: React.FC = () => {
+    return (
+        <div>
+            <h1>Sign In</h1>
+        </div>
+    )
+}
